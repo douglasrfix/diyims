aa6e6b105fbae3b4856bc2ff1254cb07bb5a719c
Douglas R Fix <douglasrfix@prairieobserver.com>
2026-01-06 16:21:02 -0600

    bump version

--- src/diyims/general_utils.py
+++ src/diyims/general_utils.py
@@ -24,7 +24,7 @@ from diyims.requests_utils import execute_request
 
 
 def get_agent() -> str:
-    agent = "0.0.0a164"  # NOTE: How to extract at run time
+    agent = "0.0.0a165"  # NOTE: How to extract at run time
 
     return agent
 
